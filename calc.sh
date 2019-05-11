ffbba8d4b3d75ed9df3124c213d68f723731b16d
hakubishin3 <goudasp2001@yahoo.co.jp>
2019-05-11 07:34:50 +0000

    model_12

--- calc.sh
+++ calc.sh
@@ -1,4 +1,4 @@
-python run.py --config configs/model_11.json --force
+python run.py --config configs/model_12.json --force
 
 sleep 60s
 gcloud compute instances stop instance-gpu --zone asia-east1-c
